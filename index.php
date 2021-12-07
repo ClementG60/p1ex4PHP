aecda4b4aaf80398e68ccb0e41e4cab38ad196cc
ClementG60 <cle_guillaume@hotmail.fr>
2021-12-07 11:38:17 +0100

    ajout code

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        $string = 'Bonjour';
+        echo $string;
+        ?>
+    </p>
+    <p>
+        <?php
+        $int = '5';
+        echo $int;
+        ?>
+    </p>
+    <p>
+        <?php
+        $float = '1.1';
+        echo $float;
+        ?>
+    </p>
+    <p>
+        <?php
+        $boolean = true;
+        echo $boolean;
+        ?>
+    </p>
+</body>
+</html>
